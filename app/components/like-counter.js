645493a4a983da14ec5f1898571e3bf89013bd72
Jonathan Fuqua <jfuqua390@gmail.com>
2016-04-05 17:34:06 -0500

    Work on like-counter component implementation

--- app/components/like-counter.js
+++ app/components/like-counter.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-});
+  score: 0,
+  up(score) {
+  this.set(`score`, this.score + 1);
+  },
+  down(score) {
+    this.set(`score`, this.score - 1);
+  }
+})
